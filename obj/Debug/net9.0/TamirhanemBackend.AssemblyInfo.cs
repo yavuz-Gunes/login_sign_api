98ceb83f3bad9dc26549beaee47c4d8dd41e519f
Yavuz Selim gÜNEŞ <129703271+yavuz-Gunes@users.noreply.github.com>
2025-02-24 11:57:36 +0300

    api_email

--- obj/Debug/net9.0/TamirhanemBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/TamirhanemBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TamirhanemBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cd0c60d24a0c3903d9521c3d07a763b686f3581")]
 [assembly: System.Reflection.AssemblyProductAttribute("TamirhanemBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TamirhanemBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
